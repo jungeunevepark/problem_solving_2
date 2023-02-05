4d8b92b142437b31086ebe500fb93bf368a4c9fb
evecomcom98 <evecomcom98@naver.com>
2023-02-05 17:56:28 +0900

    retry

--- SSSW/src/SSSW_2018_winter_2_1_16234.java
+++ SSSW/src/SSSW_2018_winter_2_1_16234.java
@@ -2,9 +2,9 @@ import java.io.*;
 import java.util.*;
 
 class Tree {
-	int x;
-	int y;
-	int age;
+	public int x;
+	public int y;
+	public int age;
 
 	Tree(int x, int y, int age) {
 		this.x = x;
@@ -12,24 +12,6 @@ class Tree {
 		this.age = age;
 	}
 
-	void setAge(int age) {
-		this.age = age;
-	}
-
-	@Override
-	public String toString() {
-		return "Tree [x=" + x + ", y=" + y + ", age=" + age + "]";
-	}
-}
-
-class Node {
-	int x;
-	int y;
-
-	Node(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
 }
 
 public class SSSW_2018_winter_2_1_16234 {
@@ -38,7 +20,10 @@ public class SSSW_2018_winter_2_1_16234 {
 	static int K;
 	static int[][] A;
 	static int[][] garden;
-	static LinkedList<Tree> t;
+	static ArrayDeque<Tree> t;
+	static ArrayDeque<Tree> add;
+	static int[] dx = { 0, 0, 1, -1, 1, -1, 1, -1 };
+	static int[] dy = { 1, -1, 0, 0, -1, 1, 1, -1 };
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -50,27 +35,28 @@ public class SSSW_2018_winter_2_1_16234 {
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		A = new int[N][N];
+		garden = new int[N][N];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(bf.readLine(), " ");
-			for (int j = 0; j < N; j++)
+			for (int j = 0; j < N; j++) {
 				A[i][j] = Integer.parseInt(st.nextToken());
+				garden[i][j] = 5;
+			}
 		}
-		t = new LinkedList<>();
+		t = new ArrayDeque<>();
+		add = new ArrayDeque<>();
+		ArrayList<Tree> temp = new ArrayList<>();
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(bf.readLine(), " ");
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
 			int z = Integer.parseInt(st.nextToken());
-			Tree tree = new Tree(x - 1, y - 1, z);
-			t.add(tree);
+			temp.add(new Tree(x - 1, y - 1, z));
 		}
-		garden = new int[N][N];
-		for (int i = 0; i < N; i++)
-			for (int j = 0; j < N; j++)
-				garden[i][j] = 5;
-		Collections.sort(t, (t1, t2) -> -Integer.compare(t1.age, t2.age));
+		Collections.sort(temp, (t1, t2) -> Integer.compare(t1.age, t2.age));
+		for (int i = 0; i < temp.size(); i++)
+			t.add(temp.get(i));
 		for (int k = 0; k < K; k++) {
-			count();
 		}
 		if (t.isEmpty())
 			sb.append(0).append("\n");
@@ -80,45 +66,38 @@ public class SSSW_2018_winter_2_1_16234 {
 	}
 
 	static void count() {
-		int[] dx = { 0, 0, 1, -1, 1, -1, 1, -1 };
-		int[] dy = { 1, -1, 0, 0, -1, 1, 1, -1 };
-		int[][] add = new int[N][N];
-		// spring
-		for (int k = t.size() - 1; k >= 0; k--) {
-			Tree tree = t.get(k);
+		int[][] plus = new int[N][N];
+		// spring + fall
+		int nx = 0;
+		int ny = 0;
+		int size = t.size();
+		while (size-- > 0) {
+			Tree tree = t.poll();
 			if (garden[tree.x][tree.y] >= tree.age) {
 				garden[tree.x][tree.y] -= tree.age;
 				tree.age += 1;
-			} else {
-				add[tree.x][tree.y] += tree.age / 2;
-				t.remove(t.get(k));
-			}
-		}
-		// System.out.println();
-		// summer
-		for (int i = 0; i < N; i++)
-			for (int j = 0; j < N; j++)
-				if (add[i][j] != 0)
-					garden[i][j] += add[i][j];
-
-		// fall
-		int size = t.size();
-		for (int i = 0; i < size; i++) {
-			Tree tree = t.get(i);
-			if (tree.age % 5 == 0) {
-				for (int d = 0; d < 8; d++) {
-					int nx = tree.x + dx[d];
-					int ny = tree.y + dy[d];
-					if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
-						t.add(new Tree(nx, ny, 1));
+				if (tree.age % 5 == 0) {
+					for (int d = 0; d < 8; d++) {
+						nx = tree.x + dx[d];
+						ny = tree.y + dy[d];
+						if (nx < 0 || ny < 0 || nx >= N || ny >= N)
+							continue;
+						add.add(new Tree(nx, ny, 1));
 					}
 				}
+				t.add(tree);
+			} else {
+				plus[tree.x][tree.y] += tree.age / 2;
 			}
+		}
+		// fall
+		while (!add.isEmpty()) {
+			Tree tr = add.poll();
+			t.addFirst(tr);
 		}
 		// winter
 		for (int i = 0; i < N; i++)
 			for (int j = 0; j < N; j++)
-				garden[i][j] += A[i][j];
-
+				garden[i][j] += A[i][j] + plus[i][j];
 	}
 }
